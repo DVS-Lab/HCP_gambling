348004978915ba0a1a1e70cd97655f2dedc0301c
mchiu91 <michelle.chiu108@gmail.com>
2017-12-12 13:29:36 -0500

    added sanity check for loops and nPPI analyses for wrapper

--- runAROMA.submit.sh
+++ runAROMA.submit.sh
@@ -9,15 +9,16 @@ MAINDATADIR=`pwd`/Data
 MAINOUTPUTDIR=`pwd`/Analysis
 cd $BASEDIR
 
-for subj in `cat sublist`; do
+for subj in `cat sublist.txt`; do
   for run in LR RL; do
     #manage number of processes vs NCORES
     while [ $(ps -ef | grep -v grep | grep runAROMA.sh | wc -l) -ge $NCORES ]; do
-	  		sleep 1m
-		done
-		bash runAROMA.sh $run $subj &
+      sleep 1m
+    done
+    bash runAROMA.sh $run $subj &
+    echo "finished running for $run $subj"
     sleep 1
-	done
+  done
 done
 
-bash runL1Gam_all.submit.sh
+#bash runL1Gam_all.submit.sh
