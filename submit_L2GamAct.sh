a53f3070f76f187c70518c6c6c3753368385a7c1
mchiu91 <30865241+mchiu91@users.noreply.github.com>
2017-12-13 00:18:31 -0500

    Delete submit_L2GamAct
    delete old wrapper defunct

--- submit_L2GamAct.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#add echo, sleep to run two sets of script so computer isn't overloaded if necessary
-#echo "sleeping for one hour"
-#sleep 1h
-for subj in 100307 100408 101107 101309 101915; do
-  bash L2_Gam_Act.sh $subj &
-  sleep 1s
-done
