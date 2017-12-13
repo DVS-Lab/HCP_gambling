a4aedc0213e6bebe95d6d076963d77414ce4d0c8
mchiu91 <30865241+mchiu91@users.noreply.github.com>
2017-12-13 12:57:31 -0500

    uncomment sanity check

--- L1_Gam_PPI.sh
+++ L1_Gam_PPI.sh
@@ -18,7 +18,7 @@ NVOLUMES=`fslnvols ${DATA}`
 #comment out sanity check when running full dataset
 #SANITY CHECK
 if [ -e ${OUTPUT}.feat/cluster_mask_zstat1.nii.gz ]; then
-  #echo "L1_Gam_PPI has been run for $subj $run"
+  echo "L1_Gam_PPI has been run for $subj $run"
   exit
 else
   rm -rf ${OUTPUT}.feat
