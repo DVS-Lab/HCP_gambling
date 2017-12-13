4c5bb4c70d974bd8d651b35b506a7e49b04254e4
mchiu91 <30865241+mchiu91@users.noreply.github.com>
2017-12-12 23:39:25 -0500

    changed runAROMA to fit hyden's
    task appears in lower level, not in wrappers

--- runAROMA.submit.sh
+++ runAROMA.submit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-task=GAMBLING
-NCORES=30
-
 BASEDIR=`pwd`
 cd ..
 MAINDATADIR=/s3/hcp
@@ -12,6 +9,7 @@ cd $BASEDIR
 for subj in `cat sublist.txt`; do
   for run in LR RL; do
     #manage number of processes vs NCORES
+    NCORES=30
     while [ $(ps -ef | grep -v grep | grep runAROMA.sh | wc -l) -ge $NCORES ]; do
       sleep 1m
     done
