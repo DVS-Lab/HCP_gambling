db90d0ea191b1ab10b5d25c67c01a0bea4ef3182
mchiu91 <30865241+mchiu91@users.noreply.github.com>
2017-12-14 01:27:53 -0500

    changed to sublist.txt and NCORES 30
    didn't uncomment nPPI because AWS had issues running those analyses

--- runL2Gam_all.submit.sh
+++ runL2Gam_all.submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NCORES=28
+NCORES=30
 
-for subj in `cat sublist`; do
+for subj in `cat sublist.txt`; do
   #manages number of processes by comparing with NCORES
   while [ $(ps -ef | grep -v grep | grep L2_Gam_Act.sh | wc -l) -ge $NCORES ]; do
     sleep 1m
@@ -10,7 +10,7 @@ for subj in `cat sublist`; do
   bash L2_Gam_Act.sh $subj &
 done
 
-for subj in `cat sublist`; do
+for subj in `cat sublist.txt`; do
   #manages number of processes by comparing with NCORES
   while [ $(ps -ef | grep -v grep | grep L2_Gam_PPI.sh | wc -l) -ge $NCORES ]; do
     sleep 1m
@@ -20,10 +20,10 @@ done
 
 #hyden's version included comparison with DMN (network PPI)
 #excluding for this round of analyses bc of time constraint
-#for subj in `cat sublist`; do
+#for subj in `cat sublist.txt`; do
     #Manages the number of jobs and cores
-    #while [ $(ps -ef | grep -v grep | grep L2_Soc_nPPI.sh | wc -l) -ge $NCORES ]; do
+    #while [ $(ps -ef | grep -v grep | grep L2_Gam_nPPI.sh | wc -l) -ge $NCORES ]; do
         #sleep 1m
     #done
-    #bash L2_Soc_nPPI.sh $subj &
+    #bash L2_Gam_nPPI.sh $subj &
 #done
