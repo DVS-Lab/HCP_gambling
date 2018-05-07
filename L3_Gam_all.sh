bffd9cc1a49a9f53cce1c3feec90a0cba2e28205
Michelle Chiu <tuh16547@temple.edu>
2018-05-07 06:39:28 +0000

    removed templates from previous test runs for Gam, ran DMN with sad behavioral cov

--- L3_Gam_all.sh
+++ L3_Gam_all.sh
@@ -17,18 +17,18 @@ OUTPUT=${MAINOUTPUTDIR}/L3_${TYPE}_${COPENUM}
 #check L3 output; avoid running analyses twice/overwriting
 #remove sanity check when running full dataset
 #SANITY CHECK WILL ONLY SHOW SCRIPT WORKS IF THE OUTPUT FOLDER DOES NOT ALREADY EXIST
-#if [ -e ${OUTPUT}.gfeat/cope1.feat/cluster_mask_zstat1.nii.gz ]; then
-  #echo "L3_Gam has been run for $TYPE $COPENUM"
-  #exit
-#else
-  #rm -rf ${OUTPUT}.gfeat
-#fi
+if [ -e ${OUTPUT}.gfeat/cope1.feat/cluster_mask_zstat1.nii.gz ]; then
+  echo "L3_Gam has been run for $TYPE $COPENUM"
+  exit
+else
+  rm -rf ${OUTPUT}.gfeat
+fi
 
 #find and replace
 #input template L3Gamtest for plain activation
 #input template L3Gamtest_sad for sad_dep
 #add _sad at the end of fsf file for nih sadness score
-ITEMPLATE=${BASEDIR}/templates/L3Gamtest.fsf
+ITEMPLATE=${BASEDIR}/templates/L3Gam187.fsf
 OTEMPLATE=${MAINOUTPUTDIR}/L3_${TYPE}_${COPENUM}.fsf
 sed -e 's@OUTPUT@'$OUTPUT'@g' \
 -e 's@TYPE@'$TYPE'@g' \
